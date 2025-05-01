a612431471b5dad17d178da446bd618210fb9a4d
zainebt002 <zeineb.tayari@esprit.tn>
2025-05-01 20:46:26 +0100

    Merge et changes zaineb

--- config/bundles.php
+++ config/bundles.php
@@ -15,5 +15,4 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
-    Flasher\Symfony\FlasherSymfonyBundle::class => ['all' => true],
 ];
